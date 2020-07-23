42364a9f130950d76e54750a9a88b52d3ade42be
udant <61499772+udant@users.noreply.github.com>
2020-07-23 17:17:08 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,53 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
-import ReadScreen from './screens/ReadScreen';
-import WriteScreen from './screens/WriteScreen';
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      text: '',
-      displaytext: '',
-      definition: '',
-      word: '',
-      lexicalCategory: '',
-    };
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.inputBox}
-          onChangeText={(text) => {}}
-          multiline={true}
-        />
-        <Button title="submit story" />
-      </View>
-    );
-  }
+import ReadScreen from './screens/ReadScreen'
+import WriteScreen from './screens/WriteScreen'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import AppHeader from './screens/AppHeader'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+                <AppHeader />
+            </View>,
+    <AppContainer />
+
+  );
 }
+const readIcon = ({ tintColor }) => (
+  <Icon name='comments' size={30} color={tintColor} />
+)
+
+
+const profileIcon = ({ tintColor }) => (
+  <Icon name='user' size={25} color={tintColor} />
+)
+
 const TabNavigator = createBottomTabNavigator({
-  Read:{screen:ReadScreen},
-  Write:{screen:WriteScreen},
+  Read:{screen:ReadScreen,
+    defaultnavigationOptions: {
+      tabBarIcon: readIcon
+    }
+  },
+  Write:{screen:WriteScreen,
+    defaultnavigationOptions: {
+      tabBarIcon: profileIcon
+    }
+  
+  }
+ /* defaultNavigationOptions :{
+        //Function 
+        header: ({scene, previous, navigation}) => {
+          console.debug("scene: ", scene)
+      }
+
+  }
+  */
 });
 
 const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
-  /* container: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },*/
-  inputBox: {
-    marginTop: 10,
-    width: '90%',
-    alignSelf: 'center',
-    height: 389,
-    textAlign: 'center',
-    borderWidth: 4,
-  },
+  }
 });
+/* defaultNavigationOptions: {
+      title: "title",
+      header: ({scene, previous, navigation}) => {
+          console.debug("scene: ", scene)
+      },*/
